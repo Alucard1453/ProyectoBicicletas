f93f61aade5242fd3807a64695526de4cbe8254a
Alucard1453 <djorgealbertola@gmail.com>
2019-10-03 12:35:21 -0500

    Precarga de imagenes

--- openbike/user.js
+++ openbike/user.js
@@ -57,3 +57,29 @@ function getMarcas(){
   xmlhttp.open("GET", "JSON_Marcas.php", true);
   xmlhttp.send();
 }
+
+//Muestra imagen cargada en la alta
+$(document).ready(function(){
+  $(function() {
+   $('#file-input').change(function(e) {
+      addImage(e); 
+    });
+ 
+    function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+     
+      if (!file.type.match(imageType))
+      return;
+   
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+    }
+   
+    function fileOnload(e) {
+      var result=e.target.result;
+      $('#imgSalida').attr("src",result);
+    }
+  });
+});
